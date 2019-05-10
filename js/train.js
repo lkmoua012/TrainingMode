3dc169571680c22375b0066a4b4d904eb7d34769
unknown <lkmoua@gmail.com>
2019-05-10 08:26:56 -0700

    began train js and added css

---
+++ js/train.js
@@ -0,0 +1,111 @@
+// for every club card
+
+// for every diamond card
+
+// for every heart card
+
+// for every spade card
+
+var Spade1 = {
+    color   :   'black',
+    type    :   'ace',
+    symbol  :   'spade',
+    number  :   1,
+    img     :   './assets/img/ace_of_spades.png'
+}
+
+var Spade2 = {
+    color   :   'black',
+    type    :   'two',
+    symbol  :   'spade',
+    number  :   2,
+    img     :   './assets/img/2_of_spades.png'
+}
+
+var Spade3 = {
+    color   :   'black',
+    type    :   'three',
+    symbol  :   'spade',
+    number  :   3,
+    img     :   './assets/img/3_of_spades.png'
+}
+
+var Spade4 = {
+    color   :   'black',
+    type    :   'four',
+    symbol  :   'spade',
+    number  :   4,
+    img     :   './assets/img/4_of_spades.png'
+}
+
+var Spade5 = {
+    color   :   'black',
+    type    :   'five',
+    symbol  :   'spade',
+    number  :   5,
+    img     :   './assets/img/5_of_spades.png'
+}
+
+var Spade6 = {
+    color   :   'black',
+    type    :   'six',
+    symbol  :   'spade',
+    number  :   6,
+    img     :   './assets/img/6_of_spades.png'
+}
+
+var Spade7 = {
+    color   :   'black',
+    type    :   'seven',
+    symbol  :   'spade',
+    number  :   7,
+    img     :   './assets/img/7_of_spades.png'
+}
+
+var Spade8 = {
+    color   :   'black',
+    type    :   'eight',
+    symbol  :   'spade',
+    number  :   8,
+    img     :   './assets/img/8_of_spades.png'
+}
+
+var Spade9 = {
+    color   :   'black',
+    type    :   'nine',
+    symbol  :   'spade',
+    number  :   9,
+    img     :   './assets/img/9_of_spades.png'
+}
+
+var Spade10 = {
+    color   :   'black',
+    type    :   'ten',
+    symbol  :   'spade',
+    number  :   10,
+    img     :   './assets/img/10_of_spades.png'
+}
+
+var Spade11 = {
+    color   :   'black',
+    type    :   'jack',
+    symbol  :   'spade',
+    number  :   10,
+    img     :   './assets/img/jack_of_spades.png'
+}
+
+var Spade12 = {
+    color   :   'black',
+    type    :   'queen',
+    symbol  :   'spade',
+    number  :   10,
+    img     :   './assets/img/queen_of_spades.png'
+}
+
+var Spade13 = {
+    color   :   'black',
+    type    :   'king',
+    symbol  :   'spade',
+    number  :   10,
+    img     :   './assets/img/king_of_spades.png'
+}
